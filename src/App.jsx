ab5125f8510e76d4c0122233060f235a5b7615da
선아 <msa200827@gmail.com>
2025-09-10 22:06:44 +0900

    feat: 기본 구조 수정

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,21 @@
+import styled from "styled-components";
+
+const Wrapper = styled.div`
+  display: flex;
+  justify-content: center;
+`;
+
+const Box = styled.div`
+  height: 100vh;
+  width: 30%;
+  border: 1px solid #000;
+`;
+
 function App() {
-  return <div>안전 마을</div>;
+  return (
+    <Wrapper>
+      <Box>안전 마을</Box>
+    </Wrapper>
+  );
 }
 export default App;
