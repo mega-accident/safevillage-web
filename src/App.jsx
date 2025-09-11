fcbfc8dc0c65cd4e9de8e72b7876e02bf653d34e
선아 <msa200827@gmail.com>
2025-09-11 21:32:01 +0900

    feat: reset css 추가

--- src/App.jsx
+++ src/App.jsx
@@ -1,22 +1,19 @@
 import styled from "styled-components";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import BottomNav from "./pages/BottomNav";
+import { GlobalStyle } from "./styles/GlobalStyle";
 
 const Wrapper = styled.div`
   display: flex;
   justify-content: center;
   align-items: center;
   min-height: 100vh;
-  background: #f7f7f7;
 `;
 
 const Box = styled.div`
   width: 375px;
   height: 812px;
   border: 1px solid #000;
-  border-radius: 40px;
-  background: #fff;
-  box-shadow: 0 8px 40px rgba(0, 0, 0, 0.12);
   display: flex;
   align-items: centeer;
   justify-content: center;
@@ -25,11 +22,14 @@ const Box = styled.div`
 
 function App() {
   return (
-    <Wrapper>
-      <Box>
-        <BottomNav />
-      </Box>
-    </Wrapper>
+    <>
+      <GlobalStyle />
+      <Wrapper>
+        <Box>
+          <BottomNav />
+        </Box>
+      </Wrapper>
+    </>
   );
 }
 export default App;
--- src/pages/BottomNav.jsx
+++ src/pages/BottomNav.jsx
@@ -3,16 +3,15 @@ import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 
 const NavWrapper = styled.nav`
   display: flex;
-  color: #eee;
 `;
 
 const BottomNav = () => {
   return (
-    <nav className="wrapper">
+    <NavWrapper>
       <div>목록</div>
       <div>홈</div>
       <div>프로필</div>
-    </nav>
+    </NavWrapper>
   );
 };
 export default BottomNav; //다른 곳에도 링크 부를 수 있도록
